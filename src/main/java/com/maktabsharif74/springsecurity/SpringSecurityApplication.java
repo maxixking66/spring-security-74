6ea6d068172ab1db5bcf73244a289d68bdb366bd
maxixking66 <maxixking66@gamil.com>
2022-10-13 13:05:38 +0330

    use security context strategy

--- src/main/java/com/maktabsharif74/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/maktabsharif74/springsecurity/SpringSecurityApplication.java
@@ -2,12 +2,14 @@ package com.maktabsharif74.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
 }
--- src/main/java/com/maktabsharif74/springsecurity/contorller/PageController.java
+++ src/main/java/com/maktabsharif74/springsecurity/contorller/PageController.java
@@ -1,5 +1,6 @@
 package com.maktabsharif74.springsecurity.contorller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,21 @@ public class PageController {
     @GetMapping("/")
     public ModelAndView getHome() {
         ModelAndView modelAndView = new ModelAndView("home");
+
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        Thread.sleep(5000);
+                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                        System.out.println(authentication.getName());
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+
         String currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
         modelAndView.addObject("currentUsername", currentUsername);
         return modelAndView;
