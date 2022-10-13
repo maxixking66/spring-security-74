65d3ec65fef4c86e93f894cc2cee2e252720bda1
maxixking66 <maxixking66@gamil.com>
2022-10-13 11:39:25 +0330

    use different way for configuring userDetailService and password encoder

--- src/main/java/com/maktabsharif74/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/maktabsharif74/springsecurity/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.maktabsharif74.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -16,13 +17,28 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails userDetails = User
+                .withUsername("mohsen")
+                .password("123456")
+                .authorities("read").build();
+
+        userDetailsManager.createUser(userDetails);
+
+        auth.userDetailsService(userDetailsManager);
+
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
         http.formLogin();
         http.httpBasic();
     }
 
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
 
@@ -34,7 +50,7 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         userDetailsManager.createUser(userDetails);
 
         return userDetailsManager;
-    }
+    }*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
