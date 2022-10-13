7742501d5628ff375d60178d1075821b13f78a76
maxixking66 <maxixking66@gamil.com>
2022-10-13 11:30:26 +0330

    override default config for httpSecurity

--- src/main/java/com/maktabsharif74/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/maktabsharif74/springsecurity/config/ProjectConfig.java
@@ -2,6 +2,8 @@ package com.maktabsharif74.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,7 +12,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
