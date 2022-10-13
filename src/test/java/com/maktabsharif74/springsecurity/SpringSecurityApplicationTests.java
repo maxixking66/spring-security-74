b4036275a869f3c7690e1907619e5fd9170ea5f5
maxixking66 <maxixking66@gamil.com>
2022-10-13 08:36:24 +0330

    init

---
+++ src/test/java/com/maktabsharif74/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maktabsharif74.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
